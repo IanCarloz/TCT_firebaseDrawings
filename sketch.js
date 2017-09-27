86a7ac169c0e2f8be7ce7dbc1af52cd3ab3f1c89
IanCarloz <iancarloz27g@gmail.com>
2017-09-26 22:44:42 -0500

    first scribble version

--- sketch.js
+++ sketch.js
@@ -27,5 +27,11 @@ function draw() {
     }
     drawing.push(point);
   }
-  // console.log(drawing);
+  beginShape();
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  for (var i = 0; i < drawing.length; i++) {
+    vertex(drawing[i].x, drawing[i].y);
+  }
 }
