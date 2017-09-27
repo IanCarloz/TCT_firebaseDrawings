6cb5e3e9f51ac060a1add4c3fdf905c58d414801
IanCarloz <iancarloz27g@gmail.com>
2017-09-26 22:07:53 -0500

    first commit, drawing is an array of points objects

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var database;
+var drawing = [];
+
+function setup() {
+  canvas = createCanvas(200, 200);
+
+  var config = {
+    apiKey: "AIzaSyAmyuals0m159WWRgl7YbsDStId6z7aG7g",
+    authDomain: "tct-drawingdb.firebaseapp.com",
+    databaseURL: "https://tct-drawingdb.firebaseio.com",
+    projectId: "tct-drawingdb",
+    storageBucket: "",
+    messagingSenderId: "571550653960"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database();
+  console.log(database);
+}
+
+function draw() {
+  background(0);
+
+  if (mouseIsPressed) {
+    var point = {
+      x: mouseX,
+      y: mouseY
+    }
+    drawing.push(point);
+  }
+  // console.log(drawing);
+}
